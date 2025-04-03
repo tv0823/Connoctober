84379c66b5e8402b795c0697c4b84bc535089fdf
Omri123S <Omri10.schwartz@gmail.com>
2025-04-03 18:38:23 +0300

    class Helper

--- Helper.py
+++ Helper.py
@@ -1,8 +1,8 @@
 import User from *
 class Helper(User):
-    def __init__(self,helpType = None):
-        if helpType is None:
-            self.help_types = []
+    def __init__(self,helpType = []):
+        self.help_types = helpType
+
         else:
             self.help_types = helpType
     def updateHelp(self, operatingln):
