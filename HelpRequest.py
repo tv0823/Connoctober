6641552fbdd9390522328f842d8fc3e051abb68e
Omri123S <Omri10.schwartz@gmail.com>
2025-04-03 19:35:32 +0300

    class HelpRequest

--- HelpRequest.py
+++ HelpRequest.py
@@ -1,9 +1,9 @@
 from User import *
 class HelpRequest:
-    def init(self, requester_name, request_details):
+    def __init__(self, requester_name, request_details):
         self.requester_name = requester_name
         self.request_details = request_details
 
     def openRequest(self):
-        print(f"בקשה חדשה מ: {self.requester_name}")
-        print(f"תיאור הבקשה: {self.request_details}")
+        print(f"New request from: {self.requester_name}")
+        print(f"Description of the request: {self.request_details}")
